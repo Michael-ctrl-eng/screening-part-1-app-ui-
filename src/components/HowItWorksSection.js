c62ad85af42f5e7936b38a6f229e814e21927e87
Michael <michael.makram.zm@gmail.com>
2025-01-31 16:27:02 +0200

    Update HowItWorksSection.js

--- src/components/HowItWorksSection.js
+++ src/components/HowItWorksSection.js
@@ -3,9 +3,9 @@ import StepCard from './StepCard';
 import './HowItWorksSection.css';
 
 const steps = [
-    { stepNumber: "01", title: "Create an Account", description: "Sign up and create your account in just a few clicks.", imageSrc: "/images/computer.png" },
-    { stepNumber: "02", title: "Choose the Course", description: "Browse and choose from a variety of courses.", imageSrc: "/images/headphone.png" },
-    { stepNumber: "03", title: "Start Learning", description: "Start learning at your own pace with expert instructors.", imageSrc: "/images/students.png" }
+    { stepNumber: "01", title: "Create an Account", description: "Sign up and create your account in just a few clicks.", imageName: "computer.png" }, // imageName prop is used now
+    { stepNumber: "02", title: "Choose the Course", description: "Browse and choose from a variety of courses.", imageName: "headphone.png" }, // imageName prop is used now
+    { stepNumber: "03", title: "Start Learning", description: "Start learning at your own pace with expert instructors.", imageName: "students.png" }  // imageName prop is used now
 ];
 
 function HowItWorksSection() {
@@ -19,7 +19,7 @@ function HowItWorksSection() {
                 <div className="step-connector-line"></div>
                 {steps.map((step, index) => (
                     <React.Fragment key={index}>
-                        <StepCard {...step} imageSrc={step.imageSrc}/> {/* Pass imageSrc correctly */}
+                        <StepCard {...step} /> {/* Pass all step data including imageName */}
                         {index < steps.length - 1 && <div className="step-connector"></div>}
                     </React.Fragment>
                 ))}
